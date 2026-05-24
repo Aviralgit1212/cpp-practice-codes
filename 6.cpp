3147bcea5353ae529d53a1a3449d4fe9811d9430
Aviral Singh <indusingh1213@gmail.com>
2026-05-24 22:03:44 +0530

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a,b;
+    cout<<"enter the first number"<<endl;
+    cin>>a;
+    cout<<"enter the second number"<<endl;
+    cin>>b;
+
+if(a<b)
+cout<<"second number is larger"<<endl;
+else
+cout<<"first number is larger"<<endl;
+return 0;
+
+}
